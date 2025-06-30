ccf9cc4aef9f590aa722ed0daab75e3bbd7d6027
Jungsu <rlawjdtn917@naver.com>
2025-06-30 16:38:15 +0900

    RBDO 기본 세팅 : y1 ~y3, parameter 설정

--- functions.py
+++ functions.py
@@ -32,12 +32,17 @@ def fit_surrogate( d, basis_terms, W, y_func, Z0_samples):
     return c, mean_ref, var_ref ,Psi
 
 # Design functions
-def y0(X):
-    X1 = X[..., 0]
-    X2 = X[..., 1]
-    return (X1 - 4)**3 + (X1 - 3)**8 + (X2 - 5)**4 + 10
-
 def y1(X):
     X1 = X[..., 0]
     X2 = X[..., 1]
-    return X1 + X2 - 6.45
+    return -1 + X1**2 * X2 / 20
+
+def y2(X):
+    X1 = X[..., 0]
+    X2 = X[..., 1]
+    return -1 + (X1 + X2 - 5)**2/30 + (X1 - X2 - 12)**2/ 120 
+
+def y3(X):
+    X1 = X[..., 0]
+    X2 = X[..., 1]
+    return -1 + 80 /(X1**2 +8 *X2 + 5)
--- main.py
+++ main.py
@@ -2,25 +2,27 @@ import numpy as np
 from scipy.optimize import minimize
 from whitening import compute_whitening_matrix
 from polybasis import generate_monomial_basis, compute_M_matrix
-from functions import generate_qmc_normal_samples , fit_surrogate, y0, y1
+from functions import generate_qmc_normal_samples , fit_surrogate, y1, y2, y3
 import Parameters   
 
 # 파라미터 설정
 N         = Parameters.N  #2
 S         = Parameters.S  #1
-m         = Parameters.m  #[8 , 1]
-m0        = Parameters.m[0]
-m1        = Parameters.m[1]
+m         = Parameters.m  #[3 , 4, 2]
+m1        = Parameters.m[0]
+m2        = Parameters.m[1]
+m3        = Parameters.m[2]
 mean      = np.array(Parameters.mean)
 cov      = np.array(Parameters.cov1)
 d_init    = np.array([5.0, 5.0])    # 초기 설계 변수
 
-basis_terms0 = generate_monomial_basis(N, S, m0)
 basis_terms1 = generate_monomial_basis(N, S, m1)
+basis_terms2 = generate_monomial_basis(N, S, m2)
+basis_terms2 = generate_monomial_basis(N, S, m3)
 
 mean_0 = mean - d_init # [0 0]s
-W0 = compute_whitening_matrix(N, S, m0, mean_0, cov )
 W1 = compute_whitening_matrix(N, S, m1, mean_0, cov )
+W2 = compute_whitening_matrix(N, S, m2, mean_0, cov )
 
 X_samples = generate_qmc_normal_samples(mean, cov, 128)
 d0 = d_init
@@ -28,20 +30,13 @@ d0 = d_init
 ##          MPSS에서는 여기서 부터 루프문
 
 Z0_samples = X_samples - d0
-c_0, mean_y0_ref, var_y0_ref, Psi_0 = fit_surrogate(d0, basis_terms0, W0, y0, Z0_samples)
 c_1, mean_y1_ref, var_y1_ref, Psi_1 = fit_surrogate(d0, basis_terms1, W1, y1, Z0_samples)
+c_2, mean_y2_ref, var_y2_ref, Psi_2 = fit_surrogate(d0, basis_terms2, W2, y2, Z0_samples)
 
 def objective(d):
-    Z_samples = X_samples - d
-    M0 = compute_M_matrix(Z_samples, basis_terms0)
-    Psi_new = M0 @ W0.T
-
-    A = Psi_0.T @ Psi_0
-    B = Psi_0.T @ (Psi_new @ c_0 )
-    c0_new = np.linalg.solve(A,B)
-
-    var_y0  = np.sum(c0_new[1:]**2) 
-    return np.sqrt(var_y0) / np.sqrt(var_y0_ref)
+    d1 = d[..., 0]
+    d2 = d[..., 1]
+    return -d1 + d2
 
 def constraint(d):
     Z_samples = X_samples - d
@@ -51,11 +46,9 @@ def constraint(d):
     A = Psi_1.T @ Psi_1
     B = Psi_1.T @ (Psi_new @ c_1 )
     c1_new = np.linalg.solve(A,B)
+    y_new   = c1_new @ Psi_new 
 
-    mean_y1  = c1_new[0]
-    var_y1  = np.sum(c1_new[1:]**2)
-
-    return  (3 * np.sqrt(var_y1) - mean_y1)
+    return  y_new
 
 result = minimize(
     fun=objective,
